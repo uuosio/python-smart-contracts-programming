f399f0bfc84317f2d49a7c2269e039bcb57d2cef
learnforpractice <learnforpractice@gmail.com>
2021-01-06 16:19:01 +0800

    Update 07.require_recipient.ipynb

--- init.py
+++ init.py
@@ -7,10 +7,17 @@ if os.path.exists('test.wallet'):
     os.remove('test.wallet')
 psw = wallet.create('test')
 
-# active key of wkpmdjdsztyu
-wallet.import_key('test', '5Jaz37nnxbpAiAGQEsyxtnGfCPTJFjX9Wn6zv7V41Ko6DXSqhd9')
+# python_contract
 # active key of ceyelqpjeeia
 wallet.import_key('test', '5JfZz1kXF8TXsxQgwfsvZCUBeTQefYSsCLDSbSPmnbKQfFmtBny')
+
+# test_account1
+# active key of wkpmdjdsztyu
+wallet.import_key('test', '5Jaz37nnxbpAiAGQEsyxtnGfCPTJFjX9Wn6zv7V41Ko6DXSqhd9')
+
+# test_account2
+# active key of ebvjmdibybgq
+wallet.import_key('test', '5KiVDjfHMHXzxrcLqZxGENrhCcCXBMSXP7paPbJWiMCDRMbStsF')
 
 uuosapi.set_node('http://127.0.0.1:8888')
 uuosapi.set_node('https://api.testnet.eos.io')
@@ -19,6 +26,7 @@ config.setup_eos_test_network()
 
 python_contract = config.python_contract
 test_account1 = 'wkpmdjdsztyu'
+test_account2 = 'ebvjmdibybgq'
 
 def run_test_code(code):
     code = uuosapi.mp_compile(python_contract, code)
